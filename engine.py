1266d4ba382c97a829db1ab8735fac432ac296d9
Virgula Leal <aqueleleal@gmail.com>
2018-12-18 17:36:44 -0200

    Pondering Monsters and Shin Assaults!

--- engine.py
+++ engine.py
@@ -1,7 +1,8 @@
 import libtcodpy as libtcod
 
-from entity import Entity
+from entity import Entity, get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
+from game_states import GameStates
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
 from render_functions import clear_all, render_all
@@ -30,7 +31,7 @@ def main():
 		"light_ground": libtcod.Color(200, 180, 50)
 	}
 
-	player = Entity(0, 0, "@", libtcod.white)
+	player = Entity(0, 0, "@", libtcod.white, "Player", blocks = True)
 	entities = [player]
 
 	libtcod.console_set_custom_font("arial10x10.png", libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -48,6 +49,8 @@ def main():
 
 	key = libtcod.Key()
 	mouse = libtcod.Mouse()
+
+	game_state = GameStates.PLAYERS_TURN
 
 	while not libtcod.console_is_window_closed():
 		libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
@@ -69,15 +72,32 @@ def main():
 		exit = action.get("exit")
 		fullscreen = action.get("fullscreen")
 
-		if move:
+		if move and game_state == GameStates.PLAYERS_TURN:
 			dx, dy = move
-			if not game_map.is_blocked(player.x + dx, player.y + dy):
-				player.move(dx, dy)
+			destination_x = player.x + dx
+			destination_y = player.y + dy
 
-				fov_recompute = True
+			if not game_map.is_blocked(destination_x, destination_y):
+				target = get_blocking_entities_at_location(entities, destination_x, destination_y)
+
+				if target:
+					print("You kick the " + target.name + " in the shins, much to its annoyance!")
+				else:
+					player.move(dx, dy)
+
+					fov_recompute = True
+
+				game_state = GameStates.ENEMY_TURN
 
 		if fullscreen:
 			libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+		if game_state == GameStates.ENEMY_TURN:
+			for entity in entities:
+				if entity != player:
+					print("The " + entity.name + " ponders the meaning of its existence.")
+
+			game_state = GameStates.PLAYERS_TURN
 
 		if exit:
 			return True
